964ca1ae2a0f8918bfbf03ca76e69214446260d8
Kirill Cherepanov <beaverkiria@gmail.com>
2015-04-12 14:49:21 +0300

    Deleted redundante code

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,12 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
     var isUserTypingNow = false
     var wasDotTyped = false
     let emptyDisplay = "0"
@@ -92,11 +86,6 @@ class ViewController: UIViewController {
         
         evaluator.putOpearator(sender.currentTitle!)
         displayValue = evaluator.evaluate()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
